737726649b89e24349b7a2adfa369f6798ca0e42
Joshua Desmond <JoshDesmond@users.noreply.github.com>
2019-07-19 14:43:17 -0400

    Massive updates and restructuring

--- profile.ps1
+++ profile.ps1
@@ -1,12 +1,60 @@
-Write-Host 'Hello!'
+# Logging Function
+function Print-ProfileLog {
+	param($text)
+	Write-Host $text -ForegroundColor green	
+}
 
+
+#======================
+#====== Aliases =======
+#======================
+Print-ProfileLog 'Configuring Aliases'
+New-Alias ppl Print-ProfileLog
+New-Alias which get-command
 New-Alias npp notepad++.lnk
 New-Alias pc "C:\Users\$env:username\Google Drive\Percent Complete 2017.xlsx"
+
+
+#======================
+#=== $Env Settings ====
+#======================
+ppl 'Configuring Env Settings'
+$Env:Path += ";C:\Shortcuts"
+$Env:Path += ";C:\Users\jdesmond\Documents\Neovim\bin\"
+
+# Colors:
+$colors = $host.privatedata
+$colors.ErrorBackgroundColor = "DarkGray"
+$colors.WarningBackgroundColor = "DarkGray"
+$colors.DebugBackgroundColor = "DarkGray"
+$colors.VerboseBackgroundColor = "DarkGray"
+
+# Console Config:
+$console = $host.ui.rawui
+$console.backgroundcolor = "black"
+
+
+#======================
+#== Import posh-git ===
+#======================
+ppl 'Importing Posh-Git'
+Import-Module posh-git
+
+#======================
+#===== Functions ====== 
+#======================
+ppl 'Defining Functions'
+
+# Print-NodePackages
+# Prints the current npm packages installed in the local directory
+function Print-NodePackages {
+	npm list --depth 0
+}
  
-# Colorized LS function replacement 
+# ll
+# Colorized LS function replacement. See:
 # http://mow001.blogspot.com 
 # http://stackoverflow.com/questions/138144/what-s-in-your-powershell-profile-ps1-file
-# Does not work on ubuntu bash
 function ll {
     param ($dir = ".", $all = $false) 
 
@@ -36,23 +84,24 @@ function lla {
 
 function la {ls -force}
 
-
+# Print-Colors
 # Prints a line of each color to console.
-Function Print-Colors {
+function Print-Colors {
 	[System.ConsoleColor].GetEnumValues() | ForEach-Object { Write-Host $_ -ForegroundColor $_ }
 }
 
+# Get-StringHash
 # Prints the Sha1 hash of a string
 # http://jongurgul.com/blog/get-stringhash-get-filehash/ 
-Function Get-StringHash([String] $String, $HashName = "SHA1") 
-{ 
+Function Get-StringHash([String] $String, $HashName = "SHA1") {
 	$StringBuilder = New-Object System.Text.StringBuilder 
-	[System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))|%{ 
-	[Void]$StringBuilder.Append($_.ToString("x2")) 
-} 
-$StringBuilder.ToString() 
+	[System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))|%{
+		[Void]$StringBuilder.Append($_.ToString("x2")) 
+	}
+	$StringBuilder.ToString() 
 }
 
+# Block-YouTube
 # Adds YouTube.com to the hosts file.
 Function Block-Youtube {
     $hosts = 'C:\Windows\System32\drivers\etc\hosts'
@@ -66,6 +115,7 @@ Function Block-Youtube {
     }
 }
 
+# Unblock-YouTube
 # Removes any lines from the hosts file containing Youtube.com
 Function Unblock-Youtube {
     $hosts = 'C:\Windows\System32\drivers\etc\hosts'
@@ -83,15 +133,32 @@ Function Unblock-Youtube {
     }
 }
 
-# Sets the background color of the prompt to the default powershell blue
-Function Set-Colors {
-
-}
 #======================
 #=Me Specific Commands=
 #======================
+ppl 'Defining Personal Functions'
 
 # Starts the IOTA Full Node running on localhost:14625
 Function Launch-IOTA {
 	java -jar C:\Git\iri\target\iri-1.4.1.4.jar -p 14265
 }
+
+# This doesn't work.
+Function Launch-VM {
+	$vbox_file = Get-Item "C:\Users\$env:username\VirtualBox VMs\CentOS 7\CentOS 7.vbox"
+	start-job {C:\Program Files\Oracle\VirtualBox\VBoxHeadless.exe -startvm $vbox_path -v}
+	Write-Host "If ssh fails, try the following command again more than once"
+	Write-Host "ssh -p2222 admin@127.0.0.1 -v" -ForegroundColor green
+	Write-Host "You can access the PowerShell jobs with the variable $job"
+	ssh -p2222 admin@127.0.0.1 -v
+}
+
+
+#======================
+#==== Finishing Up ====
+# Clear-Host
+Write-Host 'Hello!'
+
+# Notes and Favorited Commands:
+# Get-Command -Module PackageManagement # Prints available commands in the PackageManagement module
+# Get-Package -Provider Programs -IncludeWindowsInstaller # Shows everything installed
