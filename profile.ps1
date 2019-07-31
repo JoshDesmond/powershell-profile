1cf3b61b9bfd6f5c4321fe2ceb27c78f3c7f4e9b
joshdesmond <JoshDesmond@users.noreply.github.com>
2019-07-31 17:59:14 -0400

    Reformatting and Comments

--- profile.ps1
+++ profile.ps1
@@ -26,7 +26,7 @@ New-Alias vim nvim -Force
 New-Alias vi vim -Force
 New-Alias sha Get-StringHash -Force
 if ($isPersonal) {
-    New-Alias pc "C:\Users\$env:username\Google Drive\Percent Complete 2017.xlsx" -Force
+	New-Alias pc "C:\Users\$env:username\Google Drive\Percent Complete 2017.xlsx" -Force
 }
 
 #======================
@@ -48,8 +48,8 @@ $console.backgroundcolor = "black"
 # $Env:
 $Env:Path += ";C:\Shortcuts"
 if ($isVirtusa) {
-    $Env:Path += ";C:\Users\jdesmond\Documents\Neovim\bin\"
-    $Env:Path += ";C:\Users\jdesmond\Documents\NodeJS\node-v10.16.0-win-x64\"
+	$Env:Path += ";C:\Users\jdesmond\Documents\Neovim\bin\"
+	$Env:Path += ";C:\Users\jdesmond\Documents\NodeJS\node-v10.16.0-win-x64\"
 }
 
 #======================
@@ -70,7 +70,7 @@ Import-Module posh-git
 ppl 'Importing Chocolatey'
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+	Import-Module "$ChocolateyProfile"
 }
 
 #======================
@@ -83,36 +83,36 @@ ppl 'Defining Functions'
 function Print-NodePackages {
 	npm list --depth 0
 }
- 
+
 # ll
 # Colorized LS function replacement. See:
 # http://mow001.blogspot.com 
 # http://stackoverflow.com/questions/138144/what-s-in-your-powershell-profile-ps1-file
 function ll {
-    param ($dir = ".", $all = $false) 
+	param ($dir = ".", $all = $false) 
 
-    $origFg = $host.ui.rawui.foregroundColor 
+	$origFg = $host.ui.rawui.foregroundColor 
 
-    if ( $all ) { $toList = ls -force $dir }
-    else { $toList = ls $dir }
+	if ( $all ) { $toList = ls -force $dir }
+	else { $toList = ls $dir }
 
-    foreach ($Item in $toList) { 
-        Switch ($Item.Extension) { 
-            ".Exe" {$host.ui.rawui.foregroundColor = "Yellow"} 
-            ".cmd" {$host.ui.rawui.foregroundColor = "Red"} 
-            ".msh" {$host.ui.rawui.foregroundColor = "Red"} 
-            ".vbs" {$host.ui.rawui.foregroundColor = "Red"} 
-            Default {$host.ui.rawui.foregroundColor = $origFg}
-        } 
-        if ($item.Mode.StartsWith("d")) {$host.ui.rawui.foregroundColor = "Green"}
-        $item
-    }  
-    $host.ui.rawui.foregroundColor = $origFg 
+	foreach ($Item in $toList) { 
+		Switch ($Item.Extension) { 
+			".Exe" {$host.ui.rawui.foregroundColor = "Yellow"} 
+			".cmd" {$host.ui.rawui.foregroundColor = "Red"} 
+			".msh" {$host.ui.rawui.foregroundColor = "Red"} 
+			".vbs" {$host.ui.rawui.foregroundColor = "Red"} 
+			Default {$host.ui.rawui.foregroundColor = $origFg}
+		} 
+		if ($item.Mode.StartsWith("d")) {$host.ui.rawui.foregroundColor = "Green"}
+		$item
+	}  
+	$host.ui.rawui.foregroundColor = $origFg 
 }
 
 function lla {
-    param ( $dir=".")
-    ll $dir $true
+	param ( $dir=".")
+	ll $dir $true
 }
 
 function la {ls -force}
@@ -137,59 +137,68 @@ Function Get-StringHash([String] $String, $HashName = "SHA1") {
 # Block-YouTube
 # Adds YouTube.com to the hosts file.
 Function Block-Youtube {
-    if (-Not $isWindows) { return }
+	if (-Not $isWindows) { return }
 
-    $hosts = 'C:\Windows\System32\drivers\etc\hosts'
+	$hosts = 'C:\Windows\System32\drivers\etc\hosts'
 
-    $is_blocked = Get-Content -Path $hosts |
-    Select-String -Pattern ([regex]::Escape("youtube.com"))
+	$is_blocked = Get-Content -Path $hosts |
+	Select-String -Pattern ([regex]::Escape("youtube.com"))
 
-    If(-not $is_blocked) {
-       Add-Content -Path $hosts -Value "127.0.0.1 youtube.com"
-	   Add-Content -Path $hosts -Value "127.0.0.1 www.youtube.com"
-    }
+	If(-not $is_blocked) {
+		Add-Content -Path $hosts -Value "127.0.0.1 youtube.com"
+			Add-Content -Path $hosts -Value "127.0.0.1 www.youtube.com"
+	}
 }
 
 # Unblock-YouTube
 # Removes any lines from the hosts file containing Youtube.com
 Function Unblock-Youtube {
-    if (-not $isWindows) { return }
-    
-    $hosts = 'C:\Windows\System32\drivers\etc\hosts'
+	if (-not $isWindows) { return }
 
-    $is_blocked = Get-Content -Path $hosts |
-    Select-String -Pattern ([regex]::Escape("youtube.com"))
+	$hosts = 'C:\Windows\System32\drivers\etc\hosts'
 
-    If($is_blocked) {
-        $newhosts = Get-Content -Path $hosts |
-        Where-Object {
-            $_ -notmatch ([regex]::Escape("youtube.com"))
-        }
-		
+	$is_blocked = Get-Content -Path $hosts |
+	Select-String -Pattern ([regex]::Escape("youtube.com"))
+
+	If($is_blocked) {
+		$newhosts = Get-Content -Path $hosts |
+			Where-Object {
+				$_ -notmatch ([regex]::Escape("youtube.com"))
+			}
 		Set-Content -Path $hosts -Value $newhosts
-    }
+	}
 }
 
 # OpenWith-NotepadPlusPlus
 # Opens a file with notepad++
 Function OpenWith-NotepadPlusPlus {
-    notepad++.lnk (Get-ChildItem $args[0])
+	notepad++.lnk (Get-ChildItem $args[0])
 }
 
+# Get-History-All
+# Outputs the entire history that's saved
 Function Get-History-All {
 	cat (Get-PSReadlineOption).HistorySavePath
 }
 
+# Start-StartTranscript
+# Starts a transcription of the current powershell session at the path ${tracefile}
 Function Start-StartTranscript {
-    if (-not $isWindows) { break }
+	if (-not $isWindows) { break }
 	$tracefile="C:\Users\$env:username\Documents\WindowsPowerShell\Logs\PS-Session-$(get-date -format 'yyyyMMdd-HHmm').txt"
 	Start-Transcript -Path $tracefile -NoClobber
 }
 
+# Confirm-UserApproval
+# Prompts the user with ${PromptText} text, and exits 
 Function Confirm-UserApproval([String] $PromptText="Are you sure you would like to proceed") {
 	$confirmation = Read-Host $PromptText
 	if ($confirmation -eq 'y') {
 		# proceed
+		return True
+		}
+	else {
+		return false
 	}
 }
 
@@ -202,7 +211,8 @@ ppl 'Defining Personal Functions'
 if ($isDesktop) {
 Function Launch-IOTA {
 	java -jar C:\Git\iri\target\iri-1.4.1.4.jar -p 14265
-} }
+}
+}
 
 # This doesn't work.
 if ($isVirtusa) {
@@ -213,16 +223,18 @@ Function Launch-VM {
 	Write-Host "ssh -p2222 admin@127.0.0.1 -v" -ForegroundColor green
 	Write-Host "You can access the PowerShell jobs with the variable $job"
 	ssh -p2222 admin@127.0.0.1 -v
-}}
+}
+}
 
 if ($isVirtusa) {
 Function CentOSSH {
 	ssh -p2222 admin@127.0.0.1 -v
-}}
+}
+}
 
 # Adding an external script real quick.
 if ($isVirtusa) {
-    get-content -path C:\Shortcuts\lunatic.ps1 -raw | invoke-expression
+	get-content -path C:\Shortcuts\lunatic.ps1 -raw | invoke-expression
 }
 #======================
 #==== Finishing Up ====
